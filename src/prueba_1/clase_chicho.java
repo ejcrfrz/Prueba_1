5b2789c20db30c47bed2b5babb5e2664b8c483c2
Eduardo <eduardo.requena@pucp.pe>
2020-05-04 10:53:08 -0500

    chichex

---
+++ src/prueba_1/clase_chicho.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba_1;
+
+/**
+ *
+ * @author EDUARDO
+ */
+public class clase_chicho {
+
+    /**
+     * @return the chicho
+     */
+    public String getChicho() {
+        return chicho;
+    }
+
+    /**
+     * @param chicho the chicho to set
+     */
+    public void setChicho(String chicho) {
+        this.chicho = chicho;
+    }
+    
+    private String chicho;
+    
+}
